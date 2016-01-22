ebb4b9b92a47484f7eafc6228e23b045f91e0621
Julia Ashaeva <julia.ashaeva@7bits.it>
2016-01-22 17:11:01 +0600

    add edit creator feature

--- spec/admin_editcreator_feature.rb
+++ spec/admin_editcreator_feature.rb
@@ -4,6 +4,9 @@ require "pry"
 require "rspec/expectations"
 
 require_relative '../spec/spec_helper'
+# require_relative '../spec/commons/roots'
+require_relative '../spec/commons/useful_func'
+
 
 require_relative '../spec/commons/AdminAddEventPage'
 require_relative '../spec/commons/AdminEditCreatorPage'
@@ -16,105 +19,98 @@ require_relative '../spec/commons/LoginPage'
 describe "Edit Creator Page" do
   hRef = nil
   hRefEv = nil
+  page = nil
 
-  before (:all) do
-  
-    page = LoginPage.new(browser, true)
-    page.username_element.when_visible    
+  before (:all) do  
+    page = LoginPage.new(browser, true)    
     page.login_with( "admin@itlft.omsk", "admin" )   
     Watir::Wait.until{ browser.url == 'http://itlft.7bits.it/admin/requests' }    
-   # STDERR.puts "***1"
+   
     page = AdminAddEventPage.new(browser, true)
-    Watir::Wait.until{ browser.url == 'http://itlft.7bits.it/admin/event' }    
-
-    page.default()   
-  # STDERR.puts "***2 default"
+    page.sendRequest_element.when_visible
+    page.default()     
     page = AdminPanelEvents.new(browser, true)     
     Watir::Wait.until{ browser.url == 'http://itlft.7bits.it/admin/events'}
-  # STDERR.puts "***3 hRef"
+
     divEvents = browser.divs(class: "row bordered-bottom admin-event-padding")
     divEvents.each do |event|
       if event.div(class: "text-20 orange").text == "New Year celebration event"                        
         btnEdit = event.div(class: "col-xs-12 col-sm-3 admin-btn-margin-top").links[0]
         hRefEv = btnEdit.attribute_value("href")
         btnEdit = event.div(class: "col-xs-12 col-sm-3 admin-btn-margin-top").links[1]
-        hRef = btnEdit.attribute_value("href")
-        # STDERR.puts "*** hRef = " + hRef
-        btnEdit.click
-        browser.wait_until{browser.button(id: "js-submit-rq").exists?}
-        # STDERR.puts "*** wait_until"
+        hRef = btnEdit.attribute_value("href")        
         break
       end
-    end            
+    end   
   end
   
   context "RequesterName?" do    
     before (:each) do      
-      browser.goto hRef if (browser.url != hRef)
+      if (browser.url != hRef)
+        browser.goto hRef
+      else
+        browser.refresh
+      end
+      page = AdminEditCreatorPage.new(browser)    
+      page.name_element.when_present
+      page.sendRequest_element.when_present            
     end
 
     it "non-empty?" do        
-      page = AdminEditCreatorPage.new(browser)    
-      page.name_element.when_visible      
       page.name = ""
       page.sendRequest      
+      expect(page.result == "").to be_truthy
+      page.errorName_element.when_present
       expect(page.errorName !="").to be_truthy
     end
-    it " str = 'Z' " do
-      page = AdminEditCreatorPage.new(browser )      
+    it " str = 'Z' " do      
       page.name = "Z"
       page.sendRequest
-      expect(page.errorName !="").to be_falsey
+      page.result_element.when_visible      
+      expect(page.success && page.errorName =="").to be_truthy      
     end
     it " str = 'Я' " do
-      page = AdminEditCreatorPage.new(browser)      
       page.name = "Я"
       page.sendRequest
-      expect(page.errorName !="").to be_falsey
+      page.result_element.when_visible      
+      expect(page.success && page.errorName =="").to be_truthy
     end
     it " str = 'Zфыпававав' " do
-      page = AdminEditCreatorPage.new(browser)      
       page.name = "Zфыпававав"
       page.sendRequest
-      expect(page.errorName !="").to be_falsey
+      page.result_element.when_visible      
+      expect(page.success && page.errorName =="").to be_truthy
     end    
     it " str = 'Анна-Мария Елизавета Д'Эстрэ'" do
-      page = AdminEditCreatorPage.new(browser)      
       page.name = "Анна-Мария Елизавета Д'Эстрэ"
       page.sendRequest      
-      expect(page.errorName !="").to be_falsey
+      page.result_element.when_visible      
+      expect(page.success && page.errorName =="").to be_truthy
     end
     it "strlen = 256" do
-      page = AdminEditCreatorPage.new(browser)
-      browser.refresh      
-      page.name_element.when_visible
       page.name_element.clear
       page.name = str(255)    
-      page.sendRequest_element.when_visible
       page.sendRequest
-      expect(page.errorName !="").to be_falsey
+      page.result_element.when_visible      
+      expect(page.success && page.errorName =="").to be_truthy
     end
     it "str(257)" do
-      page = AdminEditCreatorPage.new(browser)
       page.name = str(256)
       page.sendRequest
+      expect(page.result == "").to be_truthy
+      page.errorName_element.when_present
       expect(page.errorName !="").to be_truthy
     end
-
-    it 'restore old value' do
-      page = AdminEditCreatorPage.new(browser)
-      page.name_element.clear      
+    it 'restore old value' do    
       page.name = "Mister Twister Administrator"
       page.sendRequest
-      expect(page.errorName !="").to be_falsey
+      page.result_element.when_visible      
+      expect(page.success && page.errorName =="").to be_truthy
     end
   end  
 
   context "RequesterEmail? " do    
-    before (:each) do     
-      browser.goto hRef if (browser.url != hRef)
-    end
-
+    
     it 'Positive email examples' do   
       emails = File.readlines("test_data/good_emails.txt")      
       aggregate_failures("good_emails") do
@@ -124,8 +120,8 @@ describe "Edit Creator Page" do
           page.email_element.when_visible                
           page.email = email          
           page.sendRequest  
-          page.errorEmail_element.when_visible          
-          expect(page.errorEmail != "").to be_falsey
+          page.result_element.when_visible          
+          expect(page.success && page.errorEmail == "").to be_truthy
         end
       end
     end    
@@ -138,41 +134,49 @@ describe "Edit Creator Page" do
           page = AdminEditCreatorPage.new(browser)        
           page.email_element.when_visible
           page.email = email          
-          page.sendRequest  
-          page.errorEmail_element.when_visible 
+          page.sendRequest
+          expect(page.result == "").to be_truthy  
+          page.errorEmail_element.when_present 
           expect(page.errorEmail !="").to be_truthy 
         end
       end
     end
 
     it "restore old value" do
+      browser.goto hRef         
       page = AdminEditCreatorPage.new(browser)     
       page.email_element.when_visible
       page.email = 'admin@itlft.omsk'
-      page.sendRequest                                  
-      expect(page.errorEmail !="").to be_falsey
+      page.sendRequest                        
+      page.result_element.when_visible
+      expect(page.success && page.errorEmail =="").to be_truthy
     end
   end
 
 
   context "Phone number?" do
-    before (:each) do      
-      browser.goto hRef if (browser.url != hRef)
+    before (:each) do     
+      if (browser.url != hRef)
+        browser.goto hRef
+      else
+        browser.refresh
+      end
+      page = AdminEditCreatorPage.new(browser)    
+      page.phone_element.when_present
+      page.sendRequest_element.when_present            
     end
+
     it "strlen = 256" do
-      page = AdminEditCreatorPage.new(browser)
-      browser.refresh      
-      page.phone_element.when_visible
-      page.phone_element.clear
       page.phone = str(255)    
-      page.sendRequest_element.when_visible
-      page.sendRequest
-      expect(page.errorPhone_element.text !="").to be_falsey
+      page.sendRequest      
+      page.result_element.when_visible
+      expect(page.success && page.errorPhone =="").to be_truthy
     end
     it "str(257)" do
-      page = AdminEditCreatorPage.new(browser)
       page.phone = str(256)
       page.sendRequest
+      expect(page.result == "").to be_truthy  
+      page.errorPhone_element.when_present
       expect(page.errorPhone_element.text !="").to be_truthy
     end
   end
--- spec/commons/AdminEditCreatorPage.rb
+++ spec/commons/AdminEditCreatorPage.rb
@@ -12,7 +12,7 @@ class AdminEditCreatorPage
     :email  => 'admin@itlft.omsk',
     :phone  => '+7-912-115-1013 (112)'
   }
-
+  SuccessMessageArray = ["Создатель сохранен", "Creator is saved"]
   
   text_field(:name,  name: "requesterName")   
   text_field(:email, name: "requesterEmail")  
@@ -23,10 +23,16 @@ class AdminEditCreatorPage
   div(:errorName,  class: "form-error js-error-name")
   div(:errorEmail, class: "form-error js-error-email")
   div(:errorPhone, class: "form-error js-error-phone")
-  
+  div(:result,     id: "js-response-rq") 
  
   def default(data = {})
     populate_page_with DEFAULT_DATA.merge(data)  
     sendRequest
   end  
+
+  def success     
+    self.result_element.when_present    
+    return SuccessMessageArray.include? self.result
+  end
+
 end
